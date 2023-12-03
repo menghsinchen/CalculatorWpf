10b03c7026668313e16c3824dcc9f13469e5444f
Meng-Hsin Chen <walkproudly@gmail.com>
2023-12-03 10:52:32 +0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculatorWpf
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double lastNumber, resultNumber;
+        private Operator selectedOperator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            btnOne.Click += btnNumber_Click;
+            btnTwo.Click += btnNumber_Click;
+            btnThree.Click += btnNumber_Click;
+            btnFour.Click += btnNumber_Click;
+            btnFive.Click += btnNumber_Click;
+            btnSix.Click += btnNumber_Click;
+            btnSeven.Click += btnNumber_Click;
+            btnEight.Click += btnNumber_Click;
+            btnNine.Click += btnNumber_Click;
+            btnZero.Click += btnNumber_Click;
+            btnPoint.Click += BtnPoint_Click;
+            btnPlus.Click += btnOperation_Click;
+            btnMinus.Click += btnOperation_Click;
+            btnMultiply.Click += btnOperation_Click;
+            btnDivide.Click += btnOperation_Click;
+            btnEqual.Click += BtnEqual_Click;
+            btnNegative.Click += BtnNegative_Click;
+            btnPercentage.Click += BtnPercentage_Click;            
+        }
+
+        private void btnNumber_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedNumber = (sender as Button).Content.ToString();
+            lblResult.Content = lblResult.Content.ToString() == "0" ? selectedNumber : lblResult.Content + selectedNumber;
+        }
+
+        private void BtnPoint_Click(object sender, RoutedEventArgs e)
+        {
+            lblResult.Content = lblResult.Content.ToString().Contains(".") ? lblResult.Content : lblResult.Content + ".";
+        }
+
+        private void btnOperation_Click(object sender, RoutedEventArgs e)
+        {
+            lblResult.Content = double.TryParse(lblResult.Content.ToString(), out lastNumber) ? "0" : lblResult.Content;
+            if (sender == btnPlus)
+            {
+                selectedOperator = Operator.Addition;
+            }
+            else if (sender == btnMinus)
+            {
+                selectedOperator = Operator.Sustraction;
+            }
+            else if (sender == btnMultiply)
+            {
+                selectedOperator = Operator.Multiplication;
+            }
+            else if (sender == btnDivide)
+            {
+                selectedOperator = Operator.Division;
+            }
+        }
+
+        private void btnAc_Click(object sender, RoutedEventArgs e)
+        {
+            lblResult.Content = "0";
+        }
+
+        private void BtnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(lblResult.Content.ToString(), out double newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case Operator.Addition:
+                        resultNumber = lastNumber + newNumber;
+                        break;
+                    case Operator.Sustraction:
+                        resultNumber = lastNumber - newNumber;
+                        break;
+                    case Operator.Multiplication:
+                        resultNumber = lastNumber * newNumber;
+                        break;
+                    case Operator.Division:
+                        resultNumber = lastNumber / newNumber;
+                        break;
+                    default:
+                        break;
+                }
+                lblResult.Content = resultNumber;
+                selectedOperator = Operator.Equalization;
+            }
+        }
+
+        private void BtnNegative_Click(object sender, RoutedEventArgs e)
+        {
+            lblResult.Content = double.TryParse(lblResult.Content.ToString(), out lastNumber) ? (lastNumber * -1) : lblResult.Content;
+        }
+
+        private void BtnPercentage_Click(object sender, RoutedEventArgs e)
+        {
+            lblResult.Content = double.TryParse(lblResult.Content.ToString(), out lastNumber) ? (lastNumber / 100) : lblResult.Content;
+        }
+    }
+
+    public enum Operator
+    {
+        Addition,
+        Sustraction,
+        Multiplication,
+        Division,
+        Equalization
+    }
+}
